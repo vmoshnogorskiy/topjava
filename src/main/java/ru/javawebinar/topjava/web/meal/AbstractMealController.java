91a7826612da7f60cf7be2750050b2a96fe4fd7b
Vladimir Moshnogorskiy <il75ich@yandex.ru>
2023-02-13 11:44:34 +0300

    create AbstractMealController

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
@@ -0,0 +1,46 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+public abstract class AbstractMealController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private MealService service;
+
+    public List<Meal> getAll(Integer userId) {
+        log.info("getAll");
+        return service.getAll(userId);
+    }
+
+    public Meal get(int id, Integer userId) {
+        log.info("get {}", id);
+        return service.get(id, userId);
+    }
+
+    public Meal create(Meal meal, Integer userId) {
+        log.info("create {}", meal);
+        checkNew(meal);
+        return service.create(meal, userId);
+    }
+
+    public void delete(int id, Integer userId) {
+        log.info("delete {}", id);
+        service.delete(id, userId);
+    }
+
+    public void update(Meal meal, int id, Integer userId) {
+        log.info("update {} with id={}", meal, id);
+        assureIdConsistent(meal, id);
+        service.update(meal, userId);
+    }
+}
