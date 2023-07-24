a776bb2e4f9f9bfc84292bd359d6ade14daafc37
Vladimir Moshnogorskiy <il75ich@yandex.ru>
2023-07-24 17:06:25 +0300

    hw7_1:add RootControllerTest::getMeals, add ResourceControllerTest

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -42,6 +42,30 @@ public class MealTo {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MealTo mealTo = (MealTo) o;
+
+        if (calories != mealTo.calories) return false;
+        if (excess != mealTo.excess) return false;
+        if (!id.equals(mealTo.id)) return false;
+        if (!dateTime.equals(mealTo.dateTime)) return false;
+        return description.equals(mealTo.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + dateTime.hashCode();
+        result = 31 * result + description.hashCode();
+        result = 31 * result + calories;
+        result = 31 * result + (excess ? 1 : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "MealTo{" +
                 "id=" + id +
