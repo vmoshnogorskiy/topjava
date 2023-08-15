1ec8ed793c817b982b0ebc3ff02ba5e841ec2988
Vladimir Moshnogorskiy <il75ich@yandex.ru>
2023-08-15 10:59:15 +0300

    HW10_add_validation_with_ExceptionInfoHandler_in_ajax_ui_controllers

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.util.ValidationUtil;
+import ru.javawebinar.topjava.util.exception.IllegalRequestDataException;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
@@ -42,8 +43,7 @@ public class MealUIController extends AbstractMealController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<String> createOrUpdate(@Valid Meal meal, BindingResult result) {
         if (result.hasErrors()) {
-            // TODO change to exception handler
-            return ValidationUtil.getErrorResponse(result);
+            throw new IllegalRequestDataException(ValidationUtil.getErrorResponse(result).getBody());
         }
         if (meal.isNew()) {
             super.create(meal);
